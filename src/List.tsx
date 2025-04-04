114ef2a5e13ae45e639fbb2cca78cc8814a65fa5
mimoimio <129141433+mimoimio@users.noreply.github.com>
2025-04-05 00:25:56 +0800

    learning about useState()

--- src/List.tsx
+++ src/List.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 function List() {
   const names = ["Mior", "Afif", "Faiz", "Auris", "Akun"];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <ul className="list-group">
-      {names.map((name) => (
-        <li key={name} className="list-group-item">
+      {names.map((name, index) => (
+        <li
+          key={index}
+          className={
+            selectedIndex === index
+              ? "list-group-item active"
+              : "list-group-item"
+          }
+          onClick={() => setSelectedIndex(index)}
+        >
           {name}
         </li>
       ))}
